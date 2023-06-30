35ab1833ac12c75c216bcd451ca6a9147b833909
MuseumMage <mageboer@qq.com>
2023-06-30 19:32:46 +0800

    添加vertex input description

--- VulkanTutorial/HelloTriangleApplication.cpp
+++ VulkanTutorial/HelloTriangleApplication.cpp
@@ -474,11 +474,14 @@ void HelloTriangleApplication::createGraphicsPipeline()
 
 	// Vertex input
 	VkPipelineVertexInputStateCreateInfo vertexInputInfo{};
+	auto bindingDescription = Vertex::getBindingDescription();
+	auto attributeDescriptions = Vertex::getAttributeDescriptions();
+
 	vertexInputInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
-	vertexInputInfo.vertexBindingDescriptionCount = 0;
-	vertexInputInfo.pVertexBindingDescriptions = nullptr; // Optional
-	vertexInputInfo.vertexAttributeDescriptionCount = 0;
-	vertexInputInfo.pVertexAttributeDescriptions = nullptr; // Optional
+	vertexInputInfo.vertexBindingDescriptionCount = 1;
+	vertexInputInfo.vertexAttributeDescriptionCount = static_cast<uint32_t>(attributeDescriptions.size());
+	vertexInputInfo.pVertexBindingDescriptions = &bindingDescription;
+	vertexInputInfo.pVertexAttributeDescriptions = attributeDescriptions.data();
 
 	// Input assembly
 	// VK_PRIMITIVE_TOPOLOGY_POINT_LIST: points from vertices
--- VulkanTutorial/HelloTriangleApplication.h
+++ VulkanTutorial/HelloTriangleApplication.h
@@ -11,9 +11,11 @@
 #include <vulkan/vulkan.h>
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
 #include <iostream>
 #include <optional>
 #include <vector>
+#include <array>
 
 // Vulkan Tutorial
 // Setup
@@ -37,186 +39,237 @@
 //      * Submit the recorded command buffer
 //      * Present the swap chain image
 
+struct Vertex
+{
+	glm::vec2 pos;
+	glm::vec3 color;
+
+	// Binding descriptions
+	static VkVertexInputBindingDescription getBindingDescription()
+	{
+		VkVertexInputBindingDescription bindingDescription{};
+		bindingDescription.binding = 0;
+		bindingDescription.stride = sizeof(Vertex);
+		bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+
+		return bindingDescription;
+	}
+
+	// Attribute descriptions
+	static std::array<VkVertexInputAttributeDescription, 2> getAttributeDescriptions() {
+	    std::array<VkVertexInputAttributeDescription, 2> attributeDescriptions{};
+
+		// pos
+		attributeDescriptions[0].binding = 0;
+		attributeDescriptions[0].location = 0;
+		// float: VK_FORMAT_R32_SFLOAT
+		// vec2: VK_FORMAT_R32G32_SFLOAT
+		// vec3: VK_FORMAT_R32G32B32_SFLOAT
+		// vec4: VK_FORMAT_R32G32B32A32_SFLOAT
+		// ivec2: VK_FORMAT_R32G32_SINT, a 2-component vector of 32-bit signed integers
+		// uvec4: VK_FORMAT_R32G32B32A32_UINT, a 4-component vector of 32-bit unsigned integers
+		// double: VK_FORMAT_R64_SFLOAT, a double-precision (64-bit) float
+		attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
+		attributeDescriptions[0].offset = offsetof(Vertex, pos);
+
+		// color
+		attributeDescriptions[1].binding = 0;
+		attributeDescriptions[1].location = 1;
+		attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+		attributeDescriptions[1].offset = offsetof(Vertex, color);
+
+	    return attributeDescriptions;
+	}
+};
+
 struct QueueFamilyIndices
 {
-    std::optional<uint32_t> graphicsFamily;
-    std::optional<uint32_t> presentFamily;
-    bool isComplete() {
-        return graphicsFamily.has_value() && presentFamily.has_value();
-    }
+	std::optional<uint32_t> graphicsFamily;
+	std::optional<uint32_t> presentFamily;
+	bool isComplete() {
+		return graphicsFamily.has_value() && presentFamily.has_value();
+	}
 };
 
 struct SwapChainSupportDetails
 {
-    VkSurfaceCapabilitiesKHR capabilities;
-    std::vector<VkSurfaceFormatKHR> formats;
-    std::vector<VkPresentModeKHR> presentModes;
+	VkSurfaceCapabilitiesKHR capabilities;
+	std::vector<VkSurfaceFormatKHR> formats;
+	std::vector<VkPresentModeKHR> presentModes;
 };
 
 class HelloTriangleApplication {
 public:
-    void run() 
-    {
-        initWindow();
-        initVulkan();
-        mainLoop();
-        cleanup();
-    }
+	void run()
+	{
+		initWindow();
+		initVulkan();
+		mainLoop();
+		cleanup();
+	}
 
 private:
-    GLFWwindow* window;
-    VkInstance instance;
-    
-    const uint32_t WIDTH = 800;
-    const uint32_t HEIGHT = 600;
+	GLFWwindow* window;
+	VkInstance instance;
 
-    // inflight frames
-    const int MAX_FRAMES_IN_FLIGHT = 2;
-    
-    // validation layers
-    const std::vector<const char*> validationLayers{
-        "VK_LAYER_KHRONOS_validation"
-    };
+	const uint32_t WIDTH = 800;
+	const uint32_t HEIGHT = 600;
+
+	// inflight frames
+	const int MAX_FRAMES_IN_FLIGHT = 2;
+
+	// validation layers
+	const std::vector<const char*> validationLayers{
+		"VK_LAYER_KHRONOS_validation"
+	};
 #ifdef NDEBUG
-    const bool enableValidationLayers = false;
+	const bool enableValidationLayers = false;
 #else
-    const bool enableValidationLayers = true;
+	const bool enableValidationLayers = true;
 #endif
-    // debug msg
-    VkDebugUtilsMessengerEXT debugMessenger;
-    
-    // physical device
-    VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
-    
-    // logical device
-    VkDevice device;
-    VkQueue graphicsQueue;
+	// debug msg
+	VkDebugUtilsMessengerEXT debugMessenger;
 
-    // window surface
-    VkSurfaceKHR surface;
-    VkQueue presentQueue;
+	// physical device
+	VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
 
-    // swap chain
-    VkSwapchainKHR swapChain;
-    std::vector<VkImage> swapChainImages;
-    VkFormat swapChainImageFormat;
+	// logical device
+	VkDevice device;
+	VkQueue graphicsQueue;
+
+	// window surface
+	VkSurfaceKHR surface;
+	VkQueue presentQueue;
+
+	// swap chain
+	VkSwapchainKHR swapChain;
+	std::vector<VkImage> swapChainImages;
+	VkFormat swapChainImageFormat;
 	VkExtent2D swapChainExtent;
 
-    const std::vector<const char*> deviceExtensions = {
-        VK_KHR_SWAPCHAIN_EXTENSION_NAME
-    };
+	const std::vector<const char*> deviceExtensions = {
+		VK_KHR_SWAPCHAIN_EXTENSION_NAME
+	};
 
-    // ImageViews
-    std::vector<VkImageView> swapChainImageViews;
+	// ImageViews
+	std::vector<VkImageView> swapChainImageViews;
 
-    // Graphics pipeline
-    VkPipelineLayout pipelineLayout;
-    VkPipeline graphicsPipeline;
+	// Graphics pipeline
+	VkPipelineLayout pipelineLayout;
+	VkPipeline graphicsPipeline;
 
-    // Render pass
+	// Render pass
 	VkRenderPass renderPass;
 
-    // Framebuffers
-    std::vector<VkFramebuffer> swapChainFramebuffers;
+	// Framebuffers
+	std::vector<VkFramebuffer> swapChainFramebuffers;
 
-    // Command pools
-    VkCommandPool commandPool;
-    std::vector<VkCommandBuffer> commandBuffers;
+	// Command pools
+	VkCommandPool commandPool;
+	std::vector<VkCommandBuffer> commandBuffers;
 
-    // Semaphores and Fences
+	// Semaphores and Fences
 	std::vector<VkSemaphore> imageAvailableSemaphores;
 	std::vector<VkSemaphore> renderFinishedSemaphores;
 	std::vector<VkFence> inFlightFences;
 
-    bool framebufferResized = false;
+	// Member variables
+	bool framebufferResized = false;
+	uint32_t currentFrame = 0;
 
-    uint32_t currentFrame = 0;
-    
-private:
-    void initWindow();
-    void initVulkan();
-    void mainLoop();
-    void cleanup();
-    
-    // vk instance
-    void createInstance();
-    void destroyInstance();
-    
-    // debug msg
-    void setupDebugMessenger();
-    VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger);
-    void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator);
-    void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
-    
-    static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
-        VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
-        VkDebugUtilsMessageTypeFlagsEXT messageType,
-        const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
-        void* pUserData)
-    {
-        std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
-        return VK_FALSE;
-    }
-    
-    // physical device
-    void pickPhysicalDevice();
-    
-    // logical device
-    void createLogicalDevice();
-    void destroyDevice();
-
-    // surface
-    void createSurface();
-    void destroySurface();
-
-    // swapchain
-    void createSwapChain();
-    void destroySwapChain();
-    SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device);
-    VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats);
-    VkPresentModeKHR chooseSwapPresentMode(const std::vector<VkPresentModeKHR> availablePresentModes);
-    VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities);
-
-    // ImageView
-    void createImageViews();
-    void destroyImageViews();
-
-    // Graphics Pipeline
-    void createGraphicsPipeline();
-    void destroyGraphicsPipeline();
-    VkShaderModule createShaderModule(const std::vector<char>& code);
-
-    // Render Pass
-    void createRenderPass();
-    void destroyRenderPass();
-
-    // Framebuffers
-    void createFramebuffers();
-    void destroyFramebuffers();
-
-    // Command pools
-    void createCommandPool();
-    void destroyCommandPool();
-    void createCommandBuffers();
-    void recordCommandBuffer(VkCommandBuffer commandBuffer, uint32_t imageIndex);
-
-    // Draw
-    void drawFrame();
-    void createSyncObjects();
-    void destroySyncObjects();
-
-    // Recreate swap chain
-    void recreateSwapChain();
-    void cleanupSwapChain();
-
-    // utils
-    QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
-    bool isDeviceSuitable(VkPhysicalDevice device);
-    bool checkDeviceExtensionSupport(VkPhysicalDevice device);
-
-    static void framebufferResizeCallback(GLFWwindow* window, int width, int height)
+	// Vertex
+	const std::vector<Vertex> vertices =
 	{
-        auto app = reinterpret_cast<HelloTriangleApplication*>(glfwGetWindowUserPointer(window));
+		{{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
+		{{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
+		{{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}
+	};
+
+private:
+	void initWindow();
+	void initVulkan();
+	void mainLoop();
+	void cleanup();
+
+	// vk instance
+	void createInstance();
+	void destroyInstance();
+
+	// debug msg
+	void setupDebugMessenger();
+	VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger);
+	void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator);
+	void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
+
+	static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+		VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+		VkDebugUtilsMessageTypeFlagsEXT messageType,
+		const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+		void* pUserData)
+	{
+		std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+		return VK_FALSE;
+	}
+
+	// physical device
+	void pickPhysicalDevice();
+
+	// logical device
+	void createLogicalDevice();
+	void destroyDevice();
+
+	// surface
+	void createSurface();
+	void destroySurface();
+
+	// swapchain
+	void createSwapChain();
+	void destroySwapChain();
+	SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device);
+	VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats);
+	VkPresentModeKHR chooseSwapPresentMode(const std::vector<VkPresentModeKHR> availablePresentModes);
+	VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities);
+
+	// ImageView
+	void createImageViews();
+	void destroyImageViews();
+
+	// Graphics Pipeline
+	void createGraphicsPipeline();
+	void destroyGraphicsPipeline();
+	VkShaderModule createShaderModule(const std::vector<char>& code);
+
+	// Render Pass
+	void createRenderPass();
+	void destroyRenderPass();
+
+	// Framebuffers
+	void createFramebuffers();
+	void destroyFramebuffers();
+
+	// Command pools
+	void createCommandPool();
+	void destroyCommandPool();
+	void createCommandBuffers();
+	void recordCommandBuffer(VkCommandBuffer commandBuffer, uint32_t imageIndex);
+
+	// Draw
+	void drawFrame();
+	void createSyncObjects();
+	void destroySyncObjects();
+
+	// Recreate swap chain
+	void recreateSwapChain();
+	void cleanupSwapChain();
+
+	// utils
+	QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
+	bool isDeviceSuitable(VkPhysicalDevice device);
+	bool checkDeviceExtensionSupport(VkPhysicalDevice device);
+
+	static void framebufferResizeCallback(GLFWwindow* window, int width, int height)
+	{
+		auto app = reinterpret_cast<HelloTriangleApplication*>(glfwGetWindowUserPointer(window));
 		app->framebufferResized = true;
 	}
 };
